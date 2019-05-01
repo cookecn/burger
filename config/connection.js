6e92b70f732f57d48504d5761b05eb22614a0992
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-04-30 22:15:36 -0500

    heroku push2

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     port: 3306,
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Kicker16!',
     database: 'burgers_db'
 });
 
