9d718658be6ef7845260c2a4fa1b990ba39948dc
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-05-01 18:15:01 -0500

    heroku pwease

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@
 var mysql = require('mysql');
 
 var connection;
-if (processs.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
