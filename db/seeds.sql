1206589e619ed8a692c96df8a3798528df62d3da
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-04-30 19:55:01 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers ("burger_name", devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers ("burger_name", devoured) VALUES ("Baconburger", false);
+INSERT INTO burgers ("burger_name", devoured) VALUES ("Veganburger", false);
